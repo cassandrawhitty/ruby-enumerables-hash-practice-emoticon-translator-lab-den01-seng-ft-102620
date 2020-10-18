c39d11a086d4d9df7a07e84d2d5dac657fa886df
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-18 23:50:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_japanese_emoticon(file_name, emoticon)
   names_and_emojis = load_library(file_name)
   names_and_emojis.each do |emoji| 
-    if languages_and_emojis[:english] == emoticon
+    if languages_and_emojis.values[:english] == emoticon
       binding.pry
       return languages_and_emojis[:japanese]
     end
