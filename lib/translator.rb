b37adbadc9ba8effaaf6d658a001684dd4ecd9b1
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-18 23:50:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,8 @@ def get_japanese_emoticon(file_name, emoticon)
   names_and_emojis = load_library(file_name)
   names_and_emojis.each do |emoji| 
     if languages_and_emojis[:english] == emoticon
+       binding.pry
       return languages_and_emojis[:japanese]
-      binding.pry
     end
   end
   return "Sorry, that emoticon was not found"
